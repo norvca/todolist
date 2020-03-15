d6be1317332ae039434e8a5595df076d2e798a3d
norvca <norvca@gmail.com>
2020-03-15 23:52:09 +0800

    Integrate SVG icon into webpack flow

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -4,4 +4,9 @@ import './modules/HeaderModule/RandomTask';
 import './modules/HeaderModule/SearchBar';
 import './modules/HeaderModule/LevelChanger';
 import './modules/HeaderModule/ThemeChanger';
+
+// 图标
+import './modules/iconfont';
+
+// CSS
 import '../sass/main.scss';
