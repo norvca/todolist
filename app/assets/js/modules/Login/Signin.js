f6938d04e8bfd881b09e3f0f5d63255c611fc336
norvca <norvca@gmail.com>
2020-03-16 18:55:49 +0800

    Extract login modal from signin.js file

--- app/assets/js/modules/Login/Signin.js
+++ app/assets/js/modules/Login/Signin.js
@@ -1,115 +1,31 @@
-// 加载数据库模块
-import {backendDB as db} from "../BackendDB";
+// 模态框实例
+import { createSingleModal } from './modal';
 
-// 因为单一职责原则，把创建对象函数和形成单例的函数分开写
-// 生成登陆模态框
-const createLoginModal = () => {
-  // 创建模态框
-  const deleteModal = document.createElement("div");
-  deleteModal.classList.add("login", "fade");
-  deleteModal.innerHTML = `<div class='login__box'>
-                             <div class='login__signin'>
-                               <p class='login__signin__welcome'>欢迎登陆PureTodo!</p>
-                               <input class="login__signin__input" type="text" placeholder="用户名 / 邮箱"/>
-                               <div class="signinUsername-error error"></div>
-                               <input class="login__signin__input" type="text" placeholder="密码"/>
-                               <div class="signinPassword-error error"></div>
-                               <a class='login__signin__button'>登陆</a>
-                               <div class="login__bottom">
-                                 <a class="login__exit" href="# ">退出</a>
-                                 <span>|</span>
-                                 <a class="login__changeState" href="# ">注册新账号</a>
-                               </div>
-                             </div>
+// 创建单例模态框
+function createSingleLoginModal() {
+  // 生成模态框的实例
+  const singleModal = createSingleModal();
 
-                             <div class='login__signup hidden'>
-                               <p class='login__signup__welcome'>欢迎注册新账户!</p>
-                               <input class="login__signup__input signupUsername" type="text" placeholder="用户名 / 邮箱设置"/>
-                               <div class="signupUsername-error error"></div>
-                               <input class="login__signup__input signupPassword" type="text" placeholder="密码设置"/>
-                               <div class="signupPassword-error error"></div>
-                               <a class='login__signup__button'>注册</a>
-                               <div class="login__bottom">
-                                 <a class="login__exit" href="# ">退出</a>
-                                 <span>|</span>
-                                 <a class="login__changeState" href="# ">返回登陆栏</a>
-                               </div>
-                             </div>
-                           </div>`;
-  // 添加到 DOM 结构里
-  document.body.appendChild(deleteModal);
-  return deleteModal;
-};
+  // 让模态框可见
+  singleModal.classList.add('login--visible');
+}
 
-// 单例函数
-const getSingle = function(fn) {
-  let instance;
-  return function() {
-    return instance || (instance = fn());
-  };
-};
-
-// 生成登陆模态框的闭包
-const createSingleModal = getSingle(createLoginModal);
-
-// 对外输出接口
-const signin = {
-  // 创建单例模态框
-  createSingleLoginModal() {
-    // 生成模态框的实例
-    const singleModal = createSingleModal();
-
-
-    // 让模态框可见
-    singleModal.classList.add("login--visible");
-  },
-
-  // 提交注册按钮
-  // signup(e) {
-
-  // },
-
-  // // 失焦验证是否合法
-  // nameVertify(e) {
-  //   if (e.target.classList.contains('signupUsername')) {
-  //     const signupName = document.querySelector('.signupUsername');
-  //     const signupPass = document.querySelector('.signupPassword');
-  //     const signupName_error = document.querySelector('.signupUsername-error');
-
-  //     if (signupName.value == "") {
-  //       signupName.style.border = "1px solid #da635d";
-  //       signupName_error.innerHTML = "用户名不能为空!";
-  //       return false;
-  //     } else if (signupName.value.length >= 16 || signupName.value.length <= 3) {
-  //       signupName.style.border = "1px solid #da635d";
-  //       signupName_error.innerHTML = "长度为4-16个字符";
-  //       return false;
-  //     } else {
-  //       signupName.style.border = "1px solid #ccc";
-  //       signupName_error.innerHTML = "";
-  //     }
-  //   }
-  // },
-
-  // 切换成注册功能
-  changeState(e) {
-    const signin = document.querySelector(".login__signin");
-    const signup = document.querySelector(".login__signup");
-    if (e.target.classList.contains("login__changeState")) {
-      signin.classList.toggle("hidden");
-      signup.classList.toggle("hidden");
-    }
-  },
-
-  // 退出模态框
-  exit(e) {
-    const login = document.querySelector(".login");
-    if (e.target.classList.contains("login__exit")) {
-      login.classList.remove("login--visible");
-    }
+// 切换成注册功能
+function changeState(e) {
+  const signin = document.querySelector('.login__signin');
+  const signup = document.querySelector('.login__signup');
+  if (e.target.classList.contains('login__changeState')) {
+    signin.classList.toggle('hidden');
+    signup.classList.toggle('hidden');
   }
-};
+}
 
+// 退出模态框
+function exit(e) {
+  const login = document.querySelector('.login');
+  if (e.target.classList.contains('login__exit')) {
+    login.classList.remove('login--visible');
+  }
+}
 
-
-export {signin};
+export { createSingleLoginModal, changeState, exit };
---
+++ app/assets/js/modules/Login/modal.js
@@ -0,0 +1,67 @@
+import * as signin from './Signin';
+import { signup } from './Signup';
+
+// 创建模态框
+function createModal() {
+  const modal = document.createElement('div');
+  modal.classList.add('login', 'fade');
+  modal.innerHTML = `<div class='login__box'>
+                             <div class='login__signin'>
+                               <p class='login__signin__welcome'>欢迎登陆PureTodo!</p>
+                               <input class="login__signin__input" type="text" placeholder="用户名 / 邮箱"/>
+                               <div class="signinUsername-error error"></div>
+                               <input class="login__signin__input" type="text" placeholder="密码"/>
+                               <div class="signinPassword-error error"></div>
+                               <a class='login__signin__button'>登陆</a>
+                               <div class="login__bottom">
+                                 <a class="login__exit" href="# ">退出</a>
+                                 <span>|</span>
+                                 <a class="login__changeState" href="# ">注册新账号</a>
+                               </div>
+                             </div>
+
+                             <div class='login__signup hidden'>
+                               <p class='login__signup__welcome'>欢迎注册新账户!</p>
+                               <input class="login__signup__input signupUsername" type="text" placeholder="用户名 / 邮箱设置"/>
+                               <div class="signupUsername-error error"></div>
+                               <input class="login__signup__input signupPassword" type="text" placeholder="密码设置"/>
+                               <div class="signupPassword-error error"></div>
+                               <a class='login__signup__button'>注册</a>
+                               <div class="login__bottom">
+                                 <a class="login__exit" href="# ">退出</a>
+                                 <span>|</span>
+                                 <a class="login__changeState" href="# ">返回登陆栏</a>
+                               </div>
+                             </div>
+                           </div>`;
+  // 添加到 DOM 结构里
+  document.body.appendChild(modal);
+  return modal;
+}
+
+// 单例函数
+function getSingle(fn) {
+  let instance;
+  return function() {
+    return instance || (instance = fn());
+  };
+}
+
+// 生成登陆模态框的闭包
+const createSingleModal = getSingle(createModal);
+
+// 显示登录模态框
+function showLoginModal() {
+  var loginBox = document.querySelector('.login__box');
+  // 登陆按钮提交
+  loginBox.addEventListener('blur', signup.vertify, true);
+  loginBox.addEventListener('click', signup.submit);
+
+  // 登陆注册栏切换
+  loginBox.addEventListener('click', signin.changeState);
+
+  // 退出登陆框
+  loginBox.addEventListener('click', signin.exit);
+}
+
+export { createSingleModal, showLoginModal };
