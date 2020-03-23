b96a2bde5a32269897c9793f347db5265ae60286
norvca <norvca@gmail.com>
2020-03-23 23:53:41 +0800

    Fix bug: Correct the username in profile icon

--- app/assets/js/modules/Login/profile.js
+++ app/assets/js/modules/Login/profile.js
@@ -1,6 +1,10 @@
 function showProfile() {
   const loginBtn = document.querySelector('.site-header__login');
   const profile = document.querySelector('.site-header__profile');
+  const username = localStorage.getItem('User-name');
+  const capital = username.slice(0, 1).toUpperCase();
+
+  profile.firstElementChild.innerText = capital;
 
   loginBtn.classList.add('hidden');
   profile.classList.remove('hidden');
