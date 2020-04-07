3ff5e0f70b8ded47d1323f192e02bd1a733d57a6
norvca <norvca@gmail.com>
2020-04-08 03:17:57 +0800

    Use wepbackPrefetch function to popup items

--- app/assets/js/modules/utils/app-handler.js
+++ app/assets/js/modules/utils/app-handler.js
@@ -2,8 +2,6 @@
 import * as sidebar from '../layout/sidebar';
 import * as todolistMain from '../layout/todolist';
 import * as todolistDetail from '../layout/detail';
-import * as deleteAllPopup from '../layout/popups/deleteAll-popup';
-// import * as loginPopup from '../layout/popups/login-popup';
 
 const loginButton = document.querySelector('.site-header__loginButton');
 const loginIcon = document.querySelector('.site-header__loginIcon');
@@ -12,30 +10,9 @@ const deleteData = document.querySelector('.sidebar__delete');
 const section = document.querySelector('.todolist');
 const detail = document.querySelector('.detail__paragraph');
 
-// header 区域
-// 显示登陆注册功能模态框
-loginButton.addEventListener('click', () => {
-  import(
-    /* webpackChunkName: "login", webpackPrefetch: true */ '../layout/popups/login-popup'
-  ).then(({createHtml}) => {
-    createHtml();
-  });
-});
-// 显示登陆注册功能模态框：手机模式
-loginIcon.addEventListener('click', () => {
-  import(
-    /* webpackChunkName: "login", webpackPrefetch: true */ '../layout/popups/login-popup'
-  ).then(({createHtml}) => {
-    createHtml();
-  });
-});
-
 // sideBar 区域
 // 左侧任务栏类别、等级导航
 sidebarContent.addEventListener('click', sidebar.navigation);
-
-// 显示删库模态框
-deleteData.addEventListener('click', deleteAllPopup.createDeletePopup);
 
 // todolist 区域
 // 修改任务标题
@@ -52,3 +29,31 @@ section.addEventListener('click', todolistMain.showTaskDetail);
 
 // 右侧任务详情区域
 detail.addEventListener('focusout', todolistDetail.refreshDetail);
+
+// 显示删库模态框
+deleteData.addEventListener('click', () => {
+  import(
+    /* webpackChunkName: "delete-all", webpackPrefetch: true */ '../layout/popups/deleteAll-popup'
+  ).then(({createDeletePopup}) => {
+    createDeletePopup();
+  });
+});
+
+// header 区域
+// 显示登陆注册功能模态框
+loginButton.addEventListener('click', () => {
+  import(
+    /* webpackChunkName: "login", webpackPrefetch: true */ '../layout/popups/login-popup'
+  ).then(({createHtml}) => {
+    createHtml();
+  });
+});
+
+// 显示登陆注册功能模态框：手机模式
+loginIcon.addEventListener('click', () => {
+  import(
+    /* webpackChunkName: "login", webpackPrefetch: true */ '../layout/popups/login-popup'
+  ).then(({createHtml}) => {
+    createHtml();
+  });
+});
