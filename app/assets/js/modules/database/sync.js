d44fe41a4a9b4e3c98804c504924ddcc1a9f14d4
norvca <norvca@gmail.com>
2020-04-16 18:47:41 +0800

    refactor(database): extract detail code from data sync function

--- app/assets/js/modules/database/sync.js
+++ app/assets/js/modules/database/sync.js
@@ -3,89 +3,61 @@ import PouchDB from 'pouchdb';
 import {userDB} from './pouchDB';
 import api from '../api/urls';
 
-const syncState = document.querySelector('.todolist__sync-state');
 let syncHandler;
 
+// 登录账号后第一次同步
 function firstSync(db, username, token) {
   const dbName = hexedDBame(username);
+  const remoteDB = getRemoteDB(api, dbName, token);
 
-  let remoteDB = new PouchDB(api.syncUrl, {
-    fetch: function (url, opts) {
-      opts.headers.set('X-Auth-CouchDB-Token', token);
-      opts.headers.set('X-CouchDB-dbName', dbName);
-      opts.credentials = 'omit';
-      return PouchDB.fetch(url, opts);
-    },
-    skip_setup: true,
-  });
-
-  syncHandler = PouchDB.sync(db, remoteDB, {
-    live: true,
-    heartbeat: false,
-    timeout: false,
-    retry: true,
-  })
-    .on('active', function (info) {
-      // replication was resumed
-      console.log(info);
-      syncState.innerText = '与云端硬盘同步数据中...';
-    })
-    .on('change', function (change) {
-      // yo, something changed!
-      console.log(change);
-      userDB.showCurrentTask();
-      syncState.innerText = '数据同步完成!';
-    })
-    .on('paused', function (info) {
-      // replication was paused, usually because of a lost connection
-      console.log(info);
-    })
-
-    .on('error', function (err) {
-      // totally unhandled error (shouldn't happen)
-      console.log(err);
-      // Token过期失效后退出持续同步请求
-      localStorage.clear();
-    });
+  syncHandler = getSyncHandler(db, remoteDB);
 }
 
+// 已经登陆则自动同步
 function reSync(db) {
   const dbName = localStorage.getItem('DB-name');
   const token = localStorage.getItem('CouchDB-auth');
+  const remoteDB = getRemoteDB(api, dbName, token);
 
-  let remoteDB = new PouchDB(api.syncUrl, {
+  syncHandler = getSyncHandler(db, remoteDB);
+}
+
+// 连接服务端数据库
+function getRemoteDB(api, dbName, token) {
+  return new PouchDB(api.syncUrl, {
     fetch: function (url, opts) {
-      opts.headers.set('X-Auth-CouchDB-Token', token);
       opts.headers.set('X-CouchDB-dbName', dbName);
+      opts.headers.set('X-Auth-CouchDB-Token', token);
       opts.credentials = 'omit';
       return PouchDB.fetch(url, opts);
     },
     skip_setup: true,
   });
+}
 
-  syncHandler = PouchDB.sync(db, remoteDB, {
+// 获取数据同步状态
+function getSyncHandler(db, remoteDB) {
+  const syncState = document.querySelector('.todolist__sync-state');
+
+  return PouchDB.sync(db, remoteDB, {
     live: true,
     heartbeat: false,
     timeout: false,
     retry: true,
   })
     .on('active', function (info) {
-      // replication was resumed
       console.log(info);
       syncState.innerText = '与云端硬盘同步数据中...';
     })
     .on('change', function (change) {
-      // yo, something changed!
       console.log(change);
       userDB.showCurrentTask();
       syncState.innerText = '数据同步完成!';
     })
     .on('paused', function (info) {
-      // replication was paused, usually because of a lost connection
       console.log(info);
     })
     .on('error', function (err) {
-      // totally unhandled error (shouldn't happen)
       console.log(err);
       // Token过期失效后退出持续同步请求
       localStorage.clear();
