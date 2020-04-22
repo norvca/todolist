b8d11302043217ca179dda106e9e45397c5749e1
norvca <norvca@gmail.com>
2020-04-23 01:33:22 +0800

    refactor(header): let functions do one thing

--- app/assets/js/modules/layout/header/level-changer.js
+++ app/assets/js/modules/layout/header/level-changer.js
@@ -1,13 +1,20 @@
-let levels = ['level-light', 'level-usual', 'level-heavy'];
-let levelColors = ['bgc-light', 'bgc-usual', 'bgc-heavy'];
+let levelArr = ['level-light', 'level-usual', 'level-heavy'];
+let levelColorArr = ['bgc-light', 'bgc-usual', 'bgc-heavy'];
 
 function changeLevel() {
-  levels = [levels[1], levels[2], levels[0]];
-  levelColors = [levelColors[1], levelColors[2], levelColors[0]];
+  alterLevel();
+  useLevel();
+}
 
+function alterLevel() {
+  levelArr = [levelArr[1], levelArr[2], levelArr[0]];
+  levelColorArr = [levelColorArr[1], levelColorArr[2], levelColorArr[0]];
+}
+
+function useLevel() {
   const levelChangeBtn = document.querySelector('.icon__level');
-  levelChangeBtn.setAttribute('class', `icon icon__level ${levels[0]}`);
-  levelChangeBtn.setAttribute('level', levelColors[0]);
+  levelChangeBtn.setAttribute('class', `icon icon__level ${levelArr[0]}`);
+  levelChangeBtn.setAttribute('level', levelColorArr[0]);
 }
 
 export {changeLevel};
