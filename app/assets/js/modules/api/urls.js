aaf6511ae8a1f37a342c70c915c765326e62ef26
norvca <norvca@gmail.com>
2020-06-04 20:54:16 +0800

    refactor(pouch-db): inline related emlemts into PouchDB class

--- app/assets/js/modules/api/urls.js
+++ app/assets/js/modules/api/urls.js
@@ -1,10 +1,10 @@
 // 同步数据前经过的 proxy 服务器
 
 const api = {
-  syncUrl: 'http://192.168.206.144:4000',
-  signinUrl: 'http://192.168.206.144:3000/api/user/login',
-  registerUrl: 'http://192.168.206.144:3000/api/user/register',
-  changeUsernameUrl: 'http://192.168.206.144:4000/api/user/changeUsername',
+  syncUrl: 'http://192.168.71.129:4000',
+  signinUrl: 'http://192.168.71.129:3000/api/user/login',
+  registerUrl: 'http://192.168.71.129:3000/api/user/register',
+  changeUsernameUrl: 'http://192.168.71.129:4000/api/user/changeUsername',
 };
 
 export default api;
--- app/assets/js/modules/database/pouchDB.js
+++ app/assets/js/modules/database/pouchDB.js
@@ -8,14 +8,6 @@ import timeStampTemplate from '../templates/timeStamp-template';
 // 加载 PouchDB 插件
 PouchDB.plugin(PouchdbFind);
 
-// 定义变量
-const toDayString = new Date().toString();
-const input = helperFunction.get_input_element();
-const level = helperFunction.get_level_element();
-const taskLists = document.querySelector('.todolist__tasks');
-const detail = document.querySelector('.detail__paragraph');
-const detailTitle = document.querySelector('.detail__title');
-
 // 创建数据库
 let pouchUser = new PouchDB('userDB');
 let pouchVisitor = new PouchDB('visitorDB');
@@ -24,11 +16,17 @@ let pouchVisitor = new PouchDB('visitorDB');
 class PouchClass {
   constructor(db) {
     this.db = db;
+    this.toDayString = new Date().toString();
+    this.input = helperFunction.get_input_element();
+    this.level = helperFunction.get_level_element();
+    this.taskLists = document.querySelector('.todolist__tasks');
+    this.detail = document.querySelector('.detail__paragraph');
+    this.detailTitle = document.querySelector('.detail__title');
   }
 
   // 添加数据到数据库
   addTask(randomContent, ramdomLevel) {
-    const task = taskCreater(randomContent, ramdomLevel);
+    const task = this.taskCreater(randomContent, ramdomLevel);
 
     this.db
       .put(task)
@@ -102,8 +100,8 @@ class PouchClass {
       taskList.appendChild(taskItemHTML);
     });
 
-    taskLists.innerHTML = '';
-    taskLists.appendChild(taskList);
+    this.taskLists.innerHTML = '';
+    this.taskLists.appendChild(taskList);
 
     // 首任务聚焦
     if (taskList.firstChild) {
@@ -144,8 +142,8 @@ class PouchClass {
           }
         });
 
-        taskLists.innerHTML = '';
-        taskLists.appendChild(taskList);
+        this.taskLists.innerHTML = '';
+        this.taskLists.appendChild(taskList);
 
         // 首任务聚焦
         if (taskList.firstChild) {
@@ -172,14 +170,14 @@ class PouchClass {
       this.db.get(id).then(doc => {
         const title = doc.title;
         const text = doc.detail;
-        detailTitle.innerText = title;
-        detail.value = text;
-        detail.placeholder = '添加任务详情...';
+        this.detailTitle.innerText = title;
+        this.detail.value = text;
+        this.detail.placeholder = '添加任务详情...';
       });
     } else {
-      detailTitle.innerText = '';
-      detail.value = '';
-      detail.placeholder = '此分类目前没有任务哦~';
+      this.detailTitle.innerText = '';
+      this.detail.value = '';
+      this.detail.placeholder = '此分类目前没有任务哦~';
     }
   }
 
@@ -201,35 +199,37 @@ class PouchClass {
         console.log(err + '删除数据库失败！');
       });
   }
-}
 
-// 任务模板
-function taskCreater(randomContent, ramdomLevel) {
-  const title = input.value;
-  const taskLevel = level.getAttribute('level');
-  const taskTime = new Date().toLocaleDateString();
-  const taskType = document
-    .querySelector('.sidebar__act')
-    .getAttribute('taskType');
-  const taskWeek = toDayString.slice.call(toDayString, 0, 3).toUpperCase();
+  // 任务模板
+  taskCreater(randomContent, ramdomLevel) {
+    const title = this.input.value;
+    const taskLevel = this.level.getAttribute('level');
+    const taskTime = new Date().toLocaleDateString();
+    const taskType = document
+      .querySelector('.sidebar__act')
+      .getAttribute('taskType');
+    const taskWeek = this.toDayString.slice
+      .call(this.toDayString, 0, 3)
+      .toUpperCase();
 
-  let taskConfig = {
-    _id: new Date().toISOString(),
-    title: title,
-    level: taskLevel,
-    taskTime: taskTime,
-    taskType: taskType,
-    taskWeek: taskWeek,
-    detail: null,
-  };
+    let taskConfig = {
+      _id: new Date().toISOString(),
+      title: title,
+      level: taskLevel,
+      taskTime: taskTime,
+      taskType: taskType,
+      taskWeek: taskWeek,
+      detail: null,
+    };
 
-  // 有标题等参数则说明添加随机任务
-  if (randomContent) {
-    taskConfig.title = randomContent;
-    taskConfig.level = ramdomLevel;
+    // 有标题等参数则说明添加随机任务
+    if (randomContent) {
+      taskConfig.title = randomContent;
+      taskConfig.level = ramdomLevel;
+    }
+
+    return taskConfig;
   }
-
-  return taskConfig;
 }
 
 const userDB = new PouchClass(pouchUser);
