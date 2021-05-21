3b9de8a571720269017651280b2be676aa39b9e0
norvca <norvca@gmail.com>
2021-05-21 10:36:11 +0800

    refactor: variable url link

--- app/src/js/modules/api/urls.ts
+++ app/src/js/modules/api/urls.ts
@@ -1,11 +1,12 @@
 // 同步数据前经过的 proxy 服务器
 import { IUrls } from '../interfaces/IUrls';
+const BACKEND_URL = '192.168.229.133';
 
 const api: IUrls = {
-  syncUrl: 'http://192.168.229.130:4000',
-  signInUrl: 'http://192.168.229.130:3000/api/user/login',
-  registerUrl: 'http://192.168.229.130:3000/api/user/register',
-  changeUsernameUrl: 'http://192.168.229.130:4000/api/user/changeUsername',
+  syncUrl: `http://${BACKEND_URL}:4000`,
+  signInUrl: `http://${BACKEND_URL}:3000/api/user/login`,
+  registerUrl: `http://${BACKEND_URL}:3000/api/user/register`,
+  changeUsernameUrl: `http://${BACKEND_URL}:4000/api/user/changeUsername`,
 };
 
 export default api;
