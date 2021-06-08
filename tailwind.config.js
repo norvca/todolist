da0eb5fb38c2fdcaa4a04e091eb7d89974b0e1b2
norvca <norvca@gmail.com>
2021-06-08 21:28:59 +0800

    feat(nav): html and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,18 @@ module.exports = {
         red: "#da635d",
         "red-light": "#da9798",
       },
+      width: {
+        "2/14": "14.28571%",
+        "3/14": "21.42858%",
+        "9/14": "64.28571%",
+      },
+      borderWidth: {
+        3: "3px",
+      },
+      boxShadow: {
+        left: "0px 2px 11px 0px #ccc",
+        right: "2px 4px 6px 0px #ccc",
+      },
     },
   },
   variants: {
