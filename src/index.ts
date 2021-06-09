821fd7b2fe79001bf221cf6b42d12b78122153d6
norvca <norvca@gmail.com>
2021-06-09 23:31:11 +0800

    test: change icon

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,20 @@ import "./img/sprite.svg";
 
 // TODO: TO be removed
 // Test Dark Mode
-
 document.addEventListener("DOMContentLoaded", (event) => {
   const htmlClasses = document.querySelector("html")!.classList;
   htmlClasses.remove("dark");
 
-  document.querySelector(".theme")?.addEventListener("click", function () {
+  document.querySelector(".theme")?.addEventListener("click", function (e) {
+    const svg = e.target as SVGElement;
+    const use = svg.querySelector("use")!;
+    console.log(use);
+    const iconHref = use.getAttribute("href");
+    iconHref === "#sprite_moon"
+      ? use.setAttribute("href", "#sprite_sun")
+      : use.setAttribute("href", "#sprite_moon");
+
+    // const use = svg.querySelector('use');
     htmlClasses.toggle("dark");
   });
 });
